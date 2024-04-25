834c01a6fdfa78b2a54cb048847d93776bc56668
Talya-Perets <155684002+Talya-Perets@users.noreply.github.com>
2024-04-25 22:49:51 +0300

    Update Graph.hpp

---
+++ Graph.hpp
@@ -0,0 +1,20 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+
+using namespace std;
+
+class Graph {
+public:
+    Graph();
+    void loadGraph(const vector<vector<int>>& graph_matrix);
+    void printGraph() const;
+    int count_edges() const;
+    bool isDirected() const;
+    int isWeighted() const; // Function added to check if the graph is weighted (1), unweighted and non-negative (0), or contains negative edges (-1)
+private:
+    vector<vector<int>> graph_matrix;
+};
+
+#endif
