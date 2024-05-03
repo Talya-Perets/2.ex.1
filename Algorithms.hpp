fdea4798d9df277a4a4f8d8f1ac9d86bcf4fb4d6
Talya-Perets <155684002+Talya-Perets@users.noreply.github.com>
2024-05-03 12:42:03 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -2,18 +2,19 @@
 #define ALGORITHMS_HPP
 
 #include "Graph.hpp"
+#include <string>
 
 namespace ariel {
-
     class Algorithms {
     public:
+        static int checkGraphType(const std::vector<std::vector<int>>& graph);
         static bool isConnected(const Graph& g);
+        static bool Algorithms::dfsCheckCycle(const Graph& g, int current, int parent, std::vector<bool>& visited) ;
         static std::string shortestPath(const Graph& g, int start, int end);
         static bool isContainsCycle(const Graph& g);
         static std::string isBipartite(const Graph& g);
         static bool negativeCycle(const Graph& g);
     };
-
 }
 
-#endif
+#endif // ALGORITHMS_HPP
