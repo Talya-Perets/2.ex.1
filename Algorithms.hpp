39459701d3638fe0fccd03552fc1f137de5f25ca
Talya-Perets <155684002+Talya-Perets@users.noreply.github.com>
2024-05-05 20:59:35 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -1,6 +1,12 @@
 #ifndef ALGORITHMS_HPP
 #define ALGORITHMS_HPP
 
+#include <string>
+#include <queue>
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+#include <limits>
 #include "Graph.hpp"
 
 namespace ariel {
@@ -12,6 +18,9 @@ namespace ariel {
         static bool isContainsCycle(const Graph& g);
         static std::string isBipartite(const Graph& g);
         static bool negativeCycle(const Graph& g);
+        static std::string BFS(const Graph& g, size_t start, size_t end);
+        static std::string Dijkstra(const Graph& g, size_t start, size_t end);
+        static std::string BellmanFord(const Graph& g, size_t start, size_t end);
     };
 
 }
