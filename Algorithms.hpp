bb1e10e14087ff15c444519ec181f56e990019c9
Talya-Perets <155684002+Talya-Perets@users.noreply.github.com>
2024-05-06 10:17:09 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -15,7 +15,9 @@ namespace ariel {
     public:
         static bool isConnected(const Graph& g);
         static std::string shortestPath(const Graph& g, int start, int end);
-        static bool isContainsCycle(const Graph& g);
+        static std::string isContainsCycle(const Graph& g);
+         static bool DFSUtil(const std::vector<std::vector<int>>& graph_matrix, size_t v, std::vector<bool>& visited, std::vector<int>& parent) ;
+
         static std::string isBipartite(const Graph& g);
         static bool negativeCycle(const Graph& g);
         static std::string BFS(const Graph& g, size_t start, size_t end);
